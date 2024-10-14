4d26358cbfeb9f528fe46f693bf81845d1e3d383
KhanhCan2k4 <levietkhanh2k4@gmail.com>
2024-10-14 22:48:34 +0700

    add before merge

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,6 +1,5 @@
 import express, { Response } from 'express';
 import SUser from '../services/SUser';
-import UserDTO from './../dtos/UserDTO';
 import SResponse, { ResponseStatus } from '../services/SResponse';
 import SLog, { LogType } from '../services/SLog';
 export default class UserController {
@@ -33,21 +32,21 @@ export default class UserController {
     }
 
     public static getAllUsers(request: express.Request, response: express.Response) {
-        SUser.getAllUsers((users) => {
-            const userDTOs: UserDTO[] = [];
+        // SUser.getAllUsers((users) => {
+        //     const userDTOs: UserDTO[] = [];
 
-            users.forEach(user => {
-                const userDTO = new UserDTO(user);
-                userDTOs.push(userDTO);
-            });
+        //     users.forEach(user => {
+        //         const userDTO = new UserDTO(user);
+        //         userDTOs.push(userDTO);
+        //     });
 
-            SLog.log(LogType.Info, "getAllUsers", "users", users);
-            SLog.log(LogType.Info, "getAllUsers", "userDTOs", userDTOs);
+        //     SLog.log(LogType.Info, "getAllUsers", "users", users);
+        //     SLog.log(LogType.Info, "getAllUsers", "userDTOs", userDTOs);
 
-            return response.json(SResponse.getResponse(ResponseStatus.OK, userDTOs, "get all users", response));
-        });
+        //     return response.json(SResponse.getResponse(ResponseStatus.OK, userDTOs, "get all users", response));
+        // });
     }
-
+ 
     public static getUser(request: express.Request, response: express.Response) {
 
     }
