3ec94ecb75867da3662a1e0f6ec57270bcc40556
khanhcan2k4 <levietkhanh2k4@gmail.com>
2024-10-31 19:39:29 +0800

    update config files, update api for user, include firebase, update functions in SUser

--- src/services/SMessage.ts
+++ src/services/SMessage.ts
@@ -1,3 +1,33 @@
+import Message from "../models/Message";
+import SMySQL from "./SMySQL";
+import SLog, {LogType} from "./SLog";
+import SFirebase from "./SFirebase";
+import SResponse, {ResponseStatus} from "./SResponse";
+
 export default class SMessage {
-    
+    public static storeMessage(message: Message, onNext: (result: boolean) => void) {
+        const sql = "INSERT INTO messages (`from_user_id`, `to_user_id`, `content`, `created_at`) VALUES (?,?,?,?)";
+
+        SMySQL.getConnection(connection => {
+            connection?.execute(sql, [
+                message.from_user?.id,
+                message.to_user?.id,
+                message.content,
+                new Date().getTime()
+            ], (error, result) => {
+                if (error) {
+                    onNext(false);
+                    SLog.log(LogType.Error, "storeMessage", "id: " + message.id, error);
+                    return;
+                }
+
+                // push message into firebase
+                SFirebase.pushMessage(message.from_user?.id, message.to_user?.id,
+                    () => {
+                        SLog.log(LogType.Info, "storeMessage successfully", "id: " + message.id, result);
+                        onNext(true);
+                    });
+            });
+        })
+    }
 }
