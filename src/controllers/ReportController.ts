83e7f1fdf89bd6b149e9e29cb345380ee243bf93
VanDuPlus <22211TT1357@mail.tdc.edu.vn>
2024-11-04 13:32:25 +0700

    get api user and class reports

--- src/controllers/ReportController.ts
+++ src/controllers/ReportController.ts
@@ -1,17 +1,45 @@
 import express from "express";
 import SUserReport from "../services/SUserReport";
 import SResponse, { ResponseStatus } from "../services/SResponse";
+import SClassReport from "../services/SClassReport";
 
 export default class ReportController {
   public static getAllClassReports(
     request: express.Request,
     response: express.Response
-  ) {}
-
+  ) {
+    SClassReport.getAllClassReport((classReport) => {
+      SResponse.getResponse(
+        ResponseStatus.OK,
+        classReport,
+        "get all classes reports",
+        response
+      );
+    });
+  }
   public static getClassReport(
     request: express.Request,
     response: express.Response
-  ) {}
+  ) {
+    const reportId = request.params.id; // Lấy reportId từ tham số đường dẫn
+    SClassReport.getClassReportById(reportId, (classReport) => {
+      if (classReport) {
+        SResponse.getResponse(
+          ResponseStatus.OK,
+          classReport,
+          "get class report by id success",
+          response
+        );
+      } else {
+        SResponse.getResponse(
+          ResponseStatus.Not_Found,
+          null,
+          "get class report by id unsuccessful",
+          response
+        );
+      }
+    });
+  }
 
   public static createClassReport(
     request: express.Request,
@@ -40,7 +68,27 @@ export default class ReportController {
   public static getUserReport(
     request: express.Request,
     response: express.Response
-  ) {}
+  ) {
+    const id = request.params.id;
+    SUserReport.geUserReportsById(id, (userReport) => {
+      if (userReport.length > 0) {
+        SResponse.getResponse(
+          ResponseStatus.OK,
+          userReport,
+          "get usersReport by id susscess",
+          response
+        );
+      }
+      else {
+        SResponse.getResponse(
+          ResponseStatus.Not_Found,
+          userReport,
+          "get usersReport by id unsusscess",
+          response
+        );
+      }
+    });
+  }
 
   public static createUserReport(
     request: express.Request,
--- src/services/SUserReport.ts
+++ src/services/SUserReport.ts
@@ -7,6 +7,7 @@ export default class SUserReport {
     const sql = `SELECT   
     JSON_OBJECT(
         'report_id', user_reports.id,
+         'created_at', user_reports.created_at,
         'from_user',
             JSON_OBJECT(
                 'id', from_user.id,
@@ -26,11 +27,27 @@ export default class SUserReport {
                 )
             ),
         'report_content', user_reports.content,
+          'status', user_reports.status,
+        'reports_before', 
+            CONCAT('[', GROUP_CONCAT(
+                JSON_OBJECT(
+                    'report_id', ur.id,
+                    'content', ur.content,
+                      'created_at', ur.created_at
+
+                    
+                ) 
+                ORDER BY ur.id ASC
+                SEPARATOR ',' 
+            ), ']'),
         'files',
-            CONCAT('[', GROUP_CONCAT(JSON_OBJECT(
-                'id', files.id,
-                'path', files.path
-            ) SEPARATOR ','), ']')
+            CONCAT('[', GROUP_CONCAT(
+                JSON_OBJECT(
+                    'id', files.id,
+                    'path', files.path
+                ) 
+                SEPARATOR ','
+            ), ']')
     ) AS report
 FROM user_reports
 LEFT JOIN users AS from_user ON from_user.id = user_reports.from_user_id
@@ -39,11 +56,91 @@ LEFT JOIN user_report_files AS urf ON urf.report_id = user_reports.id
 LEFT JOIN informations AS ifu ON ifu.user_id = user_reports.from_user_id
 LEFT JOIN informations AS itu ON itu.user_id = user_reports.to_user_id
 LEFT JOIN files ON files.id = urf.file_id
-GROUP BY user_reports.id`;
+LEFT JOIN user_reports AS ur ON ur.to_user_id = to_user.id AND ur.id < user_reports.id
+GROUP BY user_reports.id;
+`;
     SMySQL.getConnection((connection) => {
       connection?.execute<any[]>(sql, (err, results) => {
         if (err) {
           SLog.log(LogType.Error, "get all users", "failed to execute", err);
+          onNext([]);
+          return;
+        }
+        const userReports: UserReport[] = [];
+        results.forEach((result) => {
+          const userReport: UserReport = result;
+          userReports.push(userReport);
+        });
+        onNext(userReports);
+      });
+    });
+  }
+  public static geUserReportsById(id: string,onNext: (userReport: UserReport[]) => void) {
+const sql = `SELECT   
+  JSON_OBJECT(
+      'report_id', user_reports.id,
+       'created_at', user_reports.created_at,
+      'from_user',
+          JSON_OBJECT(
+              'id', from_user.id,
+              'full_name', from_user.full_name,
+              'information',
+              JSON_OBJECT(
+                  'point', ifu.point
+              ),
+              'avatar_of_fromUser',
+              JSON_OBJECT(
+                  'from_user_avatar', from_user_avatar.path
+              )
+          ),
+      'to_user',
+          JSON_OBJECT(
+              'id', to_user.id,
+              'full_name', to_user.full_name,
+              'information',
+              JSON_OBJECT(
+                  'point', itu.point
+              ),
+              'avatar_of_toUser',
+              JSON_OBJECT(
+                  'to_user_avatar', to_user_avatar.path
+              )
+          ),
+      'report_content', user_reports.content,
+        'status', user_reports.status,
+      'reports_before', 
+          JSON_ARRAYAGG(
+              JSON_OBJECT(
+                  'report_id', ur.id,
+                  'content', ur.content,
+                  'created_at', ur.created_at
+              )
+          ),
+      'files',
+          JSON_ARRAYAGG(
+              JSON_OBJECT(
+                  'id', files.id,
+                  'path', files.path
+              )
+          )
+  ) AS report
+FROM user_reports
+LEFT JOIN users AS from_user ON from_user.id = user_reports.from_user_id
+LEFT JOIN users AS to_user ON to_user.id = user_reports.to_user_id
+LEFT JOIN user_report_files AS urf ON urf.report_id = user_reports.id
+LEFT JOIN informations AS ifu ON ifu.user_id = user_reports.from_user_id
+LEFT JOIN informations AS itu ON itu.user_id = user_reports.to_user_id
+LEFT JOIN files AS from_user_avatar ON from_user_avatar.id = from_user.avatar_id
+LEFT JOIN files AS to_user_avatar ON to_user_avatar.id = to_user.avatar_id
+LEFT JOIN files ON files.id = urf.file_id
+LEFT JOIN user_reports AS ur ON ur.to_user_id = to_user.id AND ur.id < user_reports.id
+WHERE user_reports.id = ?
+GROUP BY user_reports.id, from_user.id, from_user.full_name, ifu.point, to_user.id, to_user.full_name, itu.point;
+ `;
+SMySQL.getConnection((connection) => {
+      connection?.execute<any[]>(sql,[id], (err, results) => {
+        if (err) {
+          SLog.log(LogType.Error, "get user", "failed to execute", err);
           onNext([]);
           return;
         }
