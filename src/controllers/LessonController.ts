fa9638e43208e92ab84914942c1fa928dc325c75
phuongnhi130920@gmail.com <phuongnhi130920@gmail.com>
2024-11-09 20:36:54 +0700

    viết api tạo lớp

--- src/controllers/LessonController.ts
+++ src/controllers/LessonController.ts
@@ -1,23 +1,52 @@
-import express, { Response } from 'express';
+import express, { Response } from "express";
+import SLesson from "../services/SLesson";
 export default class LessonController {
-   public static getLessonsInClass(request: express.Request, response: express.Response) {
+  public static getLessonsInClass(
+    request: express.Request,
+    response: express.Response
+  ) {}
 
-   }
+  public static getSchedule(
+    request: express.Request,
+    response: express.Response
+  ) {}
 
-   public static getSchedule(request: express.Request, response: express.Response) {
+  public static createLesson(
+    request: express.Request,
+    response: express.Response
+  ) {
+    // lay cac gia tri tu request body
+    const { day, started_at, duration, is_online } = request.body;
+    console.log("data: ", request.body);
 
-   }
+    // goi ham createLesson tu SLesson
+    SLesson.createLesson(
+      day,
+      started_at,
+      duration,
+      is_online,
+      (result, insertId) => {
+        if (result) {
+          response.status(201).json({
+            message: "Tao buoi hoc thanh cong",
+            lessonId: insertId,
+          });
+        } else {
+          response.status(500).json({
+            message: "Không thể tạo lớp học",
+          });
+        }
+      }
+    );
+  }
 
-   public static createLesson(request: express.Request, response: express.Response) {
+  public static updateLesson(
+    request: express.Request,
+    response: express.Response
+  ) {}
 
-   }
-
-   public static updateLesson(request: express.Request, response: express.Response) {
-
-   }
-
-   public static deleteLesson(request: express.Request, response: express.Response) {
-    
-   }
-
+  public static deleteLesson(
+    request: express.Request,
+    response: express.Response
+  ) {}
 }
--- src/services/SLesson.ts
+++ src/services/SLesson.ts
@@ -3,34 +3,38 @@ import SLog, { LogType } from "./SLog";
 import SMySQL from "./SMySQL";
 
 export default class SLesson {
-    /**
-     * viết thêm 1 api để lưu danh sách buổi học
-     */
+  /**
+   * viết thêm 1 api để lưu danh sách buổi học
+   */
 
-    /**
-     * @param onNext
-     */
+  /**
+   * @param onNext
+   */
 
-    public static createLesson(newLesson: Lesson, onNext: (result: boolean, insertId?: number) => void) {
-        const sql = "INSERT INTO lessons(class_id, day, started_at, duration, is_online) VALUES (?,?,?,?,?)";
-
-        const values = [
-            newLesson.day,
-            newLesson.started_at,
-            newLesson.duration,
-            newLesson.is_online,
-        ]
-
-        SMySQL.getConnection(connection => {
-            connection?.query(sql, values, (err, result) => {
-                if (err) {
-                    // Xử lý khi có lỗi
-                    SLog.log(LogType.Error, 'addNewLesson', 'Failed to insert new class', err);
-                    onNext(false,);
-                }
-                const insertId = (result as any).insertId || undefined;
-                onNext(true, insertId);
-            })
-        })
-    }
+  public static createLesson(
+    day: number,
+    started_at: number,
+    duration: number,
+    is_online: boolean,
+    onNext: (result: boolean, insertId?: number) => void
+  ) {
+    const sql =
+      "INSERT INTO lessons(day, started_at, duration, is_online) VALUES (?,?,?,?)";
+    SMySQL.getConnection((connection) => {
+      connection?.query(sql, [day, started_at, duration, is_online], (err, result) => {
+        if (err) {
+          // Xử lý khi có lỗi
+          SLog.log(
+            LogType.Error,
+            "addNewLesson",
+            "Failed to insert new class",
+            err
+          );
+          onNext(false);
+        }
+        const insertId = (result as any).insertId || undefined;
+        onNext(true, insertId);
+      });
+    });
+  }
 }
