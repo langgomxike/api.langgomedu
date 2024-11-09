4f9069e089d01b404bbe4f9b90e809826070657c
khanhcan2k4 <levietkhanh2k4@gmail.com>
2024-11-09 20:18:20 +0700

    update mesage controller, service

--- src/controllers/MessageController.ts
+++ src/controllers/MessageController.ts
@@ -5,6 +5,7 @@ import SResponse, {ResponseStatus} from "../services/SResponse";
 import SLog, {LogType} from "../services/SLog";
 import SMessage from "../services/SMessage";
 import Message from "../models/Message";
+import User from "../models/User";
 
 export default class MessageController {
     public static getContacts(request: express.Request, response: express.Response) {
@@ -42,12 +43,39 @@ export default class MessageController {
     }
 
     public static getMessages(request: express.Request, response: express.Response) {
+        const token: string = request?.headers?.authorization?.replace("Bearer ", "")?? "";
+        const fromUser : User = request?.body?.from_user ;
+        const toUser : User = request?.body?.to_user;
+        const batch: number = parseInt(request?.query?.batch?? "1");
 
+        if (!fromUser || !toUser) {
+            SLog.log(LogType.Info, "getMessages", "get messages unsuccessfully", "Cannot get the from_user or to_user");
+            SResponse.getResponse(ResponseStatus.Internal_Server_Error, null, "Cannot get the from user or to user", response);
+            return;
+        }
+
+        SUser.getUserByToken(token, (user) => {
+            if (!user) {
+                SLog.log(LogType.Error, "getMessages", "User not found");
+                SResponse.getResponse(ResponseStatus.Unauthorized, null, "Invalid token", response);
+                return;
+            }
+
+            SMessage.getMessages(fromUser.id, toUser.id, batch, (messages) => {
+                SResponse.getResponse(ResponseStatus.OK, messages, "get messages successfully", response);
+            });
+        });
     }
 
     public static createMessage(request: express.Request, response: express.Response) {
         const token: string = request?.headers?.authorization?.replace("Bearer ", "") ?? "";
         const message: Message = request?.body?.message;
+
+        if (!message) {
+            SLog.log(LogType.Info, "createMessage", "create message unsuccessfully", "Cannot get the message");
+            SResponse.getResponse(ResponseStatus.Internal_Server_Error, null, "Cannot get the message", response);
+            return;
+        }
 
         SUser.getUserByToken(token, (user) => {
             if (!user) {
@@ -62,7 +90,7 @@ export default class MessageController {
                     SResponse.getResponse(ResponseStatus.OK, null, "Message sent successfully", response);
                 } else {
                     SLog.log(LogType.Info, "createMessage", "create message unsuccessfully");
-                    SResponse.getResponse(ResponseStatus.Internal_Server_Error, null, "Failed", response);
+                    SResponse.getResponse(ResponseStatus.Internal_Server_Error, null, "Create message unsuccessfully", response);
                 }
             });
         });
@@ -70,9 +98,13 @@ export default class MessageController {
 
     public static deleteMessage(request: express.Request, response: express.Response) {
         const token: string = request?.headers?.authorization?.replace("Bearer ", "") ?? "";
-        const id: number = parseInt(request?.params?.id ?? "-1");
-        const fromUserStatus: boolean = request?.body?.from_user_status ?? true;
-        const toUserStatus: boolean = request?.body?.to_user_status ?? true;
+        const message: Message = request?.body?.message;
+
+        if (!message) {
+            SLog.log(LogType.Error, "deleteMessage", "delete message unsuccessfully");
+            SResponse.getResponse(ResponseStatus.Internal_Server_Error, null, "Message deleted unsuccessfully. Invalid message", response);
+            return;
+        }
 
         SUser.getUserByToken(token, (user) => {
             if (!user) {
@@ -81,7 +113,7 @@ export default class MessageController {
                 return;
             }
 
-            SMessage.deleteMessage(id, fromUserStatus, toUserStatus, (result) => {
+            SMessage.deleteMessage(message, (result) => {
                 if (result) {
                     SLog.log(LogType.Info, "deleteMessage", "delete message successfully");
                     SResponse.getResponse(ResponseStatus.OK, null, "Message deleted successfully", response);
