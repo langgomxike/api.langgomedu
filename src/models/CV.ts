9b56a161f82800ac481b6c1f805e93c8ed2dde8c
trkhang09 <dtrikhang0910@gmail.com>
2024-11-17 19:24:04 +0700

    update model

--- src/models/CV.ts
+++ src/models/CV.ts
@@ -1,21 +1,18 @@
 import Information from "./Information";
 import User from "./User";
-import Skill from "./Skill";
 
 export default class CV {
     public user: User | undefined
-    public information: Information | undefined
     public biography: string;
     public title: string;
     public approved_at: Date;
-    public skills: Skill[] = [];
+    public updated_at: Date;
 
-    constructor(user: User | undefined = undefined, information: Information | undefined = undefined, biography = "", title = "", approved_at = new Date(), skills: Skill[] = []) {
+    constructor(user: User | undefined = undefined, biography = "", title = "", approved_at : number,updated_at : number) {
         this.user = user;
-        this.information = information;
         this.biography = biography;
         this.title = title;
-        this.approved_at = approved_at;
-        this.skills = skills;
+        this.approved_at = new Date(approved_at);
+        this.updated_at = new Date(updated_at);
     }
 }
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,7 +1,7 @@
 import File from "./File";
 import Role from "./Role";
-import Information from "./Information";
-import Student from "./Student";
+import Address from "./Address";
+import Gender from "./Gender";
 
 export default class User {
     public id: string;
@@ -9,14 +9,19 @@ export default class User {
     public email: string;
     public phone_number: string;
     public password: string;
-    public information: Information | undefined;
-    public students: Student[] | undefined;
-    public is_reported: boolean;
     public token: string;
-    public avatar: File | undefined;
+    public avatar: string;
     public role: Role | undefined;
+    public hometown: string;
+    public address: Address | undefined;
+    public birthday: number;
+    public gender: Gender | undefined;
+    public point: number;
+    public bankingNumber: string;
+    public bankingCode: string;
     public created_at: number;
     public updated_at: number;
+    public roles: Role[];
 
     constructor(
         id = "",
@@ -24,23 +29,37 @@ export default class User {
         email = "",
         phone_number = "",
         password = "",
-        information: Information | undefined = undefined,
-        students: Student[] | undefined = [],
-        is_reported = false,
         token = "",
-        created_at = new Date().getTime(),
-        updated_at = new Date().getTime()
+        avatar = "",
+        role: Role | undefined = undefined,
+        hometown: string = "",
+        address: Address | undefined = undefined,
+        birthday = 0,
+        gender: Gender | undefined = undefined,
+        point: number = 0,
+        bankingNumber: string = "",
+        bankingCode: string = "",
+        created_at = 0,
+        updated_at = 0,
+        roles = []
     ) {
         this.id = id;
         this.full_name = full_name;
         this.email = email;
         this.phone_number = phone_number;
         this.password = password;
-        this.information = information;
-        this.students = students;
-        this.is_reported = is_reported;
         this.token = token;
+        this.avatar = avatar;
+        this.role = role;
+        this.hometown = hometown;
+        this.address = address;
+        this.birthday = birthday;
+        this.gender = gender;
+        this.point = point;
+        this.bankingNumber = bankingNumber;
+        this.bankingCode = bankingCode;
         this.created_at = created_at;
         this.updated_at = updated_at;
+        this.roles = roles;
     }
 }
